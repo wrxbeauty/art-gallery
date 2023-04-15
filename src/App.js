5b80e943a9a35185bc8de68ee605740897e48cf8
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-04-15 16:51:24 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch, connect} from 'react-redux'
 import './App.css';
+import Gallery from './components/Gallery'
+import ButtonBar from './components/ButtonBar'
+
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId
+})
+
+useEffect(() => {
+  dispatch(fetchData())
+}, [props.objectId, dispatch])
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  let [artDetails, setArtDetails] = useState({})
+  let [artId, setArtId] = useState(12720)
 
-export default App;
+  useEffect(() => {
+    document.title = 'Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setArtDetails(resData))
+  }, [artId])
+
+  const displayImage = () => {
+    if (!artDetails.primaryImage) {
+      return (
+        <h2>No Image!</h2>
+      )
+    }
+    return (
+      <Gallery objectImg={artDetails.primaryImage} artist={artDetails.artistDisplayName} title={artDetails.title} />
+    )
+  }
+
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }
+
+return (
+  <div className="App">
+    <h1>{artDetails.title}</h1>
+    <div style={{ 'width': '100' }}>
+      {displayImage()}
+    </div>
+    <ButtonBar handleIterate={handleIterate} />
+  </div>
+)
+}
+export default connect(mapStateToProps)(App)
